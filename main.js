1bb91d7937555931b20e74c0bf5d70f66b8e4d75
TFARKAS <thomas.farkas@ynov.com>
2022-03-15 12:43:39 +0100

    commit avant delete

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 function getValue() {
-    const input = document.getElementById("champstexte")
-    alert(input.value)
-    input.value = '';
+    return document.getElementById("champstexte").value
 }
 
 const button = document.getElementById("valider");
 button.addEventListener("click", getValue);
+button.addEventListener("click", duplic);
 
 function getValueIfEnterPressed(event) {
     if(event.key !== "Enter") return;
@@ -14,3 +13,23 @@ function getValueIfEnterPressed(event) {
 
 const relou = document.getElementById("champstexte");
 relou.addEventListener("keyup", getValueIfEnterPressed)
+
+function duplic() {
+    let getInput = getValue();
+    if (getInput === '') {
+        alert("Please enter a word")
+        return;
+    }
+    let nodeText = document.createTextNode(getInput)
+    let Taches = document.createElement("div")
+    Taches.id = "rectangle1"
+    Taches.appendChild(nodeText)
+    document.getElementById("TacheQuiBougePas").appendChild(Taches)
+    document.getElementById("champstexte").value = "";
+}
+
+
+
+
+
+
