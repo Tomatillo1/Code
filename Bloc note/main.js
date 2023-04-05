a2829eb241769190a1de09d56b46d907edc2ac08
Tomatillo1 <totofarkas@gmail.com>
2023-04-05 14:46:31 +0200

    add color

--- Bloc note/main.js
+++ Bloc note/main.js
@@ -1,10 +1,21 @@
 const textarea = document.querySelector('textarea');
+const textcolor = document.querySelector('#text-color');
 
-const saveValue = localStorage.getItem('text')
+const saveValue = localStorage.getItem('text');
+const savecolorValue = localStorage.getItem('changecolortext');
+
+textarea.value = saveValue
+textarea.style.color = savecolorValue;
+textcolor.value = savecolorValue
 
 textarea.addEventListener("input", function (event) {
     localStorage.setItem('text', event.target.value);
 });
 
-textarea.textContent = saveValue
+textcolor.addEventListener("input", function (event) {
+    textarea.style.color = event.target.value;
+    localStorage.setItem('changecolortext', event.target.value);
+})
+
+
 
