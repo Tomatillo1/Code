0e937fe70db888be45a41bde0c55b1301fb926ea
Tomatillo1 <totofarkas@gmail.com>
2023-04-07 14:41:29 +0200

    add background change color

--- Bloc note/main.js
+++ Bloc note/main.js
@@ -1,12 +1,16 @@
 const textarea = document.querySelector('textarea');
 const textcolor = document.querySelector('#text-color');
+const backgroundcolor = document.querySelector('#background-color');
 
 const saveValue = localStorage.getItem('text');
 const savecolorValue = localStorage.getItem('changecolortext');
+const savebackgroundcolorValue = localStorage.getItem('changebackgroundcolor');
 
-textarea.value = saveValue
+textarea.value = saveValue;
 textarea.style.color = savecolorValue;
-textcolor.value = savecolorValue
+textcolor.value = savecolorValue;
+textarea.style.backgroundColor = savebackgroundcolorValue;
+backgroundcolor.value = savebackgroundcolorValue;
 
 textarea.addEventListener("input", function (event) {
     localStorage.setItem('text', event.target.value);
@@ -15,7 +19,14 @@ textarea.addEventListener("input", function (event) {
 textcolor.addEventListener("input", function (event) {
     textarea.style.color = event.target.value;
     localStorage.setItem('changecolortext', event.target.value);
-})
+});
+
+backgroundcolor.addEventListener("input", function (event) {
+    textarea.style.backgroundColor = event.target.value;
+    localStorage.setItem('changebackgroundcolor', event.target.value);
+});
+
+
 
 
 
